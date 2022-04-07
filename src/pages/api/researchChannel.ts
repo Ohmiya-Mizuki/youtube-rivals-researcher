9d506e7ba2630b8bce89c92357efc61fddd3de90
your username <m.omiya@miwaza.co.jp>
2022-04-08 08:17:22 +0900

    chore: :art: 環境変数の移動

--- src/pages/api/researchChannel.ts
+++ src/pages/api/researchChannel.ts
@@ -1,6 +1,6 @@
 export const researchChannel = async (channelId: string) => {
   const res = await fetch(
-    `${process.env.YOUTUBE_API_SEARCH_URL}?key=${process.env.YOUTUBE_API_KEY}&part=snippet&channelId=${channelId}&order=viewCount`
+    `${process.env.NEXT_PUBLIC_YOUTUBE_API_SEARCH_URL}?key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&part=snippet&channelId=${channelId}&order=viewCount`
   );
   const data = await res.json()
   const bestVideoList = await data.items
