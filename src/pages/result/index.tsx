d776c888bffe226643c8f9e4e9d3002d8eb140b6
your username <m.omiya@miwaza.co.jp>
2022-04-09 16:07:58 +0900

    feat: stylesチェック

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -10,28 +10,31 @@ const Result = () => {
 
   if (error)
     return (
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>Welcome to YTRR!</h1>
-        </main>
-      </div>
+      // <div className={styles.container}>
+      //   <main className={styles.main}>
+      //     <h1 className={styles.title}>Welcome to YTRR!</h1>
+      //   </main>
+      // </div>
+      <div>hello</div>
     );
 
   if (!data)
     return (
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>Welcome to YTRR!</h1>
-        </main>
-      </div>
+      // <div className={styles.container}>
+      //   <main className={styles.main}>
+      //     <h1 className={styles.title}>Welcome to YTRR!</h1>
+      //   </main>
+      // </div>
+      <div>hello</div>
     );
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to YTRR!</h1>
-      </main>
-    </div>
+    // <div className={styles.container}>
+    //   <main className={styles.main}>
+    //     <h1 className={styles.title}>Welcome to YTRR!</h1>
+    //   </main>
+    // </div>
+    <div>hello</div>
   );
 };
 
