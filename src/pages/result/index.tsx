4f29a120ed156e4ef03fecbba050da5da4139a2a
your username <m.omiya@miwaza.co.jp>
2022-04-09 16:09:17 +0900

    feat: swrチェック

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,32 +1,29 @@
 import { useRouter } from "next/router";
-import styles from "@/styles/Home.module.css";
-import useSWR from "swr";
-import { researchChannel } from "@/pages/api/researchChannel";
 
 const Result = () => {
   const router = useRouter();
   const channelId = router.query.channelId;
-  const { data, error } = useSWR(channelId, researchChannel);
+  // const { data, error } = useSWR(channelId, researchChannel);
 
-  if (error)
-    return (
+  // if (error)
+  //   return (
       // <div className={styles.container}>
       //   <main className={styles.main}>
       //     <h1 className={styles.title}>Welcome to YTRR!</h1>
       //   </main>
       // </div>
-      <div>hello</div>
-    );
+    //   <div>hello</div>
+    // );
 
-  if (!data)
-    return (
+  // if (!data)
+  //   return (
       // <div className={styles.container}>
       //   <main className={styles.main}>
       //     <h1 className={styles.title}>Welcome to YTRR!</h1>
       //   </main>
       // </div>
-      <div>hello</div>
-    );
+    //   <div>hello</div>
+    // );
 
   return (
     // <div className={styles.container}>
